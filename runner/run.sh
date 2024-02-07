fa872e69524afa9f061be1c02b52b0ba3a0d525a
Sloane Tribble <sloane.tribble@liatrio.com>
2024-02-07 12:00:42 -0800

    Successfully implement with .env file; add gitignore

--- runner/run.sh
+++ runner/run.sh
@@ -5,7 +5,7 @@
 # Must have docker buildx to use buildkit=1.  Please use ./buildx.sh to install this.
 
 # Then store github repo "<username>/<repo-name>" (without quotes) and PAT, also without quotes, in files called "repo.txt" and "token.txt"
-DOCKER_BUILDKIT=1 docker build --secret id=TOKEN,src=token.txt --secret id=REPO,src=repo.txt .
+DOCKER_BUILDKIT=1 docker build --tag runner:latest --secret id=TOKEN,src=token.txt --secret id=REPO,src=repo.txt .
 
 # Run the runner
 docker run runner
